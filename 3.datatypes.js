f9753ac2662e72699af93f8ce0fa61fa0c778a28
ManashSwain <manash0802@gmail.com>
2025-12-18 21:58:53 +0530

    feat: datatypes

---
+++ 3.datatypes.js
@@ -0,0 +1,62 @@
+// Primitive Data types 
+
+// 1. Number 
+let a = 67 ;
+console.log(a);
+console.log(typeof a);
+
+// 2. String 
+let myname = "Manash";
+console.log(myname);
+console.log(myname);
+
+// 3. Boolean 
+let bool = true;
+console.log(bool);
+console.log(typeof bool)
+
+
+// 4. undefined
+let something;
+console.log(something);
+console.log(typeof something)
+
+// 5. null
+let myvalue = null;
+console.log(myvalue);
+console.log(typeof myvalue)
+
+// 6. BigInt 
+let output = BigInt(12) + BigInt(23);
+console.log(output);
+console.log(typeof output)
+
+// 7. symbol
+
+
+// Non primitive Data Types 
+// 1. Arrays 
+
+let myarr = [12,43,7];
+console.log(myarr);
+console.log(typeof myarr)
+
+// 2. Object 
+let myobj = {
+    id:1,
+    myname : "Manash"
+}
+console.log(myobj);
+console.log(typeof myobj);
+
+// 3. Functions 
+function greet(){
+    return 67;
+}
+console.log(greet());
+console.log(typeof greet)
+
+// 4. Date 
+let date = new Date();
+console.log(date);
+console.log(typeof date)
