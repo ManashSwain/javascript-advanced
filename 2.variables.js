cc283b6160adf1b5ddc9b1a6cf86af9505dd2580
ManashSwain <manash0802@gmail.com>
2025-12-18 18:35:37 +0530

    feat: variables

---
+++ 2.variables.js
@@ -0,0 +1,15 @@
+var myname = "alice"
+let age = 21 
+const city = 'chennai'
+
+myname = "rohan"
+age = 56 
+
+// city = "hydrabad"
+// single line comment
+/* multi
+line
+comment
+*/
+
+console.table([myname,age,city])
