b8a49ba3637e87235c152a65d35ae4d8050a75f6
ManashSwain <manash0802@gmail.com>
2025-12-19 17:34:05 +0530

    feat: Numbers

---
+++ 10.numbers.js
@@ -0,0 +1,10 @@
+let num = 56 ;
+console.log(typeof(num.toString()));
+
+let newnum = new Number(445);
+console.log(typeof newnum)
+
+// toFixed()
+// toPrecision()
+// toLocaleString()
+// toLocaleString('en-IN')
