9927b6570e27bc16abb322dd93c9b0019ec215c5
ManashSwain <manash0802@gmail.com>
2025-12-19 16:08:50 +0530

    feat: strings

---
+++ 9.strings.js
@@ -0,0 +1,36 @@
+let myname = "manash";
+console.log(myname);
+console.log(typeof myname)
+
+let newname = new String("Manash");
+console.log(newname)
+console.log(typeof newname)
+
+// string interpolation 
+
+let yourname = "Manash";
+let age = 54;
+
+console.log(`My name is ${yourname} and my age is ${age}`)
+
+// string methods 
+
+// 1. length 
+// 2. toUpperCase()
+// 3. toLowerCase()
+// 4. charAt()
+// 5. charCodeAt()
+// 6. trim()
+// 7. concat()
+// 8. replace("", "");
+// 9. includes();
+// 10. startsWith();
+// 11. endsWith();
+// 12. slice();
+// 13. slice(2,4)
+
+// Note: slice can also take negative values
+
+let mystr= "mynameismanash";
+console.log(mystr.slice(-1))
+
