989f87d747d85b95fc92bf0dabbfc70659170f5b
ManashSwain <manash0802@gmail.com>
2025-12-19 12:40:53 +0530

    feat: stack and heap

---
+++ 8.stackandheap.js
@@ -0,0 +1,16 @@
+let myscore = 89 ;
+let updatedscore = myscore ;
+updatedscore = 90 ;
+console.log(myscore);
+console.log(updatedscore)
+
+let myobj = {
+    id:8,
+    personname:"alice"
+}
+
+let newobj = myobj ;
+newobj.id = 10;
+
+console.log(myobj);
+console.log(newobj)
