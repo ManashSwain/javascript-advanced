74e32b2ea0512342e71eb3d36c48e1c1646f6a24
ManashSwain <manash0802@gmail.com>
2025-12-19 10:51:41 +0530

    feat: trickyconversions

---
+++ 5.trickyconversions.js
@@ -0,0 +1,26 @@
+// Conversions 
+
+console.log("1"+2);
+console.log(typeof ("1"+2))
+
+console.log(1+"2");
+console.log(typeof (1+"2"))
+
+console.log("1"+2+2);
+console.log(typeof ("1"+2+2))
+
+console.log(1+2+"2");
+console.log(typeof (1+2+"2"))
+
+console.log("1"+2+"2");
+console.log(typeof ("1"+2+"2"))
+
+console.log(+true);
+console.log(typeof +true)
+
+console.log(+"");
+console.log(typeof +"")
+
+let gamescore = 100 ;
+console.log(gamescore++);
+console.log(gamescore);
