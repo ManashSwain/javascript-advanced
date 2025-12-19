301b9cbdc33604c7bf5929786c2a9beb00fd9118
ManashSwain <manash0802@gmail.com>
2025-12-19 18:03:31 +0530

    feat: maths

---
+++ 11.maths.js
@@ -0,0 +1,14 @@
+console.log(Math)
+console.log(Math.random())
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.9))
+console.log(Math.abs(-78))
+console.log(Math.min(43,2,67,34));
+console.log(Math.max(43,2,67,34));
+
+// Generate a number between 30-60
+
+let min =30;
+let max = 60;
+
+console.log(Math.floor(Math.random() *(max-min+1)+min))
